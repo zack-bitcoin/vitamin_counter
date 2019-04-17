e84e34ba3ee49621a118e5d46ff4415ca83947b7
zack <zack.bitcoin@gmail.com>
2019-04-17 09:19:52 -0400

    made it even more keto.

--- src/diet.erl
+++ src/diet.erl
@@ -6,11 +6,13 @@
 doit() -> 
     %all amounts are measured in grams per day.
     [
-     {avocado, 770},%for b2 b2-0.1 and fat
-     {squash, 300},%for vitamin C-21
-     {chicken, 170},%for protein
-     {cabbage, 65},% for vitamin k  k-76
-     {peanuts, 60},%for omega 6,
-     {sardines, 50},%for calcium and b12 
-     {pumpkin_seeds, 30}% from the squash
+     {avocado, 970},%for b2 b2-0.1 and fat
+     %{squash, 300},%for vitamin A
+     {chicken, 130},%for protein
+     %{sardines, 50},%for b12 and calcium
+     {egg, 150},%for b12. need 1/2 eggshell per day of calcium as well
+     {peanuts, 60},%for manganese*2.1
+     {squash, 20},%for vitamin A
+     {flax, 0}
+     %{pumpkin_seeds, 30}% from the squash
     ].
