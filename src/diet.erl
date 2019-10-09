aa51348d7ae871f89a694f61da37484d32da438a
zack-bitcoin <zack.bitcoin@gmail.com>
2019-10-09 00:28:19 -0300

    add banana onions and beef

--- src/diet.erl
+++ src/diet.erl
@@ -6,13 +6,15 @@
 doit() -> 
     %all amounts are measured in grams per day.
     [
-     {avocado, 970},%for b2 b2-0.1 and fat
-     %{squash, 300},%for vitamin A
-     {chicken, 130},%for protein
-     %{sardines, 50},%for b12 and calcium
-     {egg, 150},%for b12. need 1/2 eggshell per day of calcium as well
-     {peanuts, 60},%for manganese*2.1
-     {squash, 20},%for vitamin A
-     {flax, 0}
-     %{pumpkin_seeds, 30}% from the squash
+     {beef_80_20, 200},
+     {egg, 400},
+     {squash, 200},
+     {dry_split_peas, 80},
+     {cabbage, 80},
+     {sunflower_oil, 100},
+     {butter, 20},
+     {banana, 100},
+     {onion, 200},
+     %{cheese, 50},
+     {pumpkin_seeds, 30}
     ].
